e4a1a7cfb63857068f702da176e6e97e892d1cef
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2023-11-26 18:54:57 +0000

    Set up skeleton project.

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+//! A driver for the register interface on the Nuvoton NAU88C22 CODEC
+//!
+//! Works in I2C mode only, for now.
+
+// SPDX-FileCopyrightText: 2023 Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
